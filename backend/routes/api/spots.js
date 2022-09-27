bc929dec18fde68cb65b15049ac761abbb33f201
michaellacey <mikelacey123123@gmail.com>
2022-09-26 21:18:12 -0400

    get all spots in progress, adding seeds for it.

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,16 +1,48 @@
 const express = require('express')
-const { User, SpotImages, Spot, Reviews, ReviewImages, Booking } = require('../../db/models');
+const { User, SpotImage, Spot, Review, ReviewImage, Booking } = require('../../db/models');
 const router = express.Router();
 
-// app.use(express.json())
+// const cats = await Cat.findAll({ where: { name: 'Lucy' }, include: Owner })
+// cats[0].Owner
+// Get routes
+// app.patch('/authors/:authorId/books', async (req, res) => {
+//     const author = await Author.findOne({
+//         include: { model: Book },
+//         where: {
+//             id: req.params.authorId
+//         }
+//     });
 
-router.get('/', (req,res) => {
-    const spots = Spot.findAll()
-    res.json(spots)
-})
+router.get('/', async (req,res) => {
+    console.log("You're in the right url")
+    const spots = await Spot.findAll({
+        include: [
+            {
+                model: Review
+            },
+            {
+                model: SpotImage
+            }
+        ]
+
+    })
+    // console.log(spots)
+    let spotsList = [];
+
+    spots.forEach(spot => {
+        // console.log(spot)
+        // console.log(spot.toJSON())
+        spotsList.push(spot)
+    })
+console.log('review: ', spotsList[0].Reviews)
+
+    return res.json(spots)
+});
 
 
 
+
+// Post routes
 
 
 module.exports = router;
