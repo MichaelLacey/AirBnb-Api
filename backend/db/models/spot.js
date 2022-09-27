6a96c9a6ffb2fc8fd25cdfcc68af1759854b2b84
michaellacey <mikelacey123123@gmail.com>
2022-09-27 18:38:04 -0400

    Created 2 routes, updated spots models

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -31,15 +31,43 @@ module.exports = (sequelize, DataTypes) => {
   }
   Spot.init({
     ownerId: DataTypes.INTEGER,
-    address: DataTypes.STRING,
-    city: DataTypes.STRING,
-    state: DataTypes.STRING,
-    country: DataTypes.STRING,
-    lat: DataTypes.DECIMAL,
-    lng: DataTypes.DECIMAL,
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    price: DataTypes.DECIMAL
+    address: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    city: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    country: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    lat: {
+      type:DataTypes.DECIMAL,
+      allowNull: false
+    },
+    lng: {
+      type:DataTypes.DECIMAL,
+      allowNull: false
+    },
+    name: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type:DataTypes.DECIMAL,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'Spot',
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,8 +1,9 @@
 const e = require('express');
-const express = require('express')
+const express = require('express');
 const { User, SpotImage, Spot, Review, ReviewImage, Booking } = require('../../db/models');
 const router = express.Router();
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 //  GET
 
 // ALL SPOTS
@@ -40,8 +41,8 @@ router.get('/', async (req, res) => {
         delete spotsList[destroy].SpotImages;
         destroy++
     })
-
     return res.json(spotsList);
+
 });
 
 // ALL SPOTS FOR CURRENT USER
@@ -51,33 +52,83 @@ router.get('/current', async (req, res) => {
 
 // Post routes
 
+// const validateSignup = [
+//     check('address')
+//       .exists({ checkFalsy: true })
+//       .withMessage("Street address is required"),
+//     check('city')
+//       .exists({ checkFalsy: true })
+//       .withMessage("City is required"),
+//     check('state')
+//       .exists({ checkFalsy: true })
+//       .withMessage("State is required"),
+//     check('country')
+//       .exists({ checkFalsy: true })
+//       .withMessage("Country is required"),
+//     check('lat')
+//        .withMessage("Latitude is not valid"),
+//     check('lng')
+//         .withMessage("Longitude is not valid"),
+//     check('name')
+//         .isLength({max: 49})
+//         .withMessage("Name must be less than 50 characters"),
+//     check('description')
+//     .exists({ checkFalsy: true })
+//         .withMessage("Description is required"),
+//     check('price')
+//     .exists({ checkFalsy: true })
+//         .withMessage("Price per day is required"),
+//     handleValidationErrors
+//   ];
 //Create new spot
-router.post('/', async (req,res) => {
+router.post('/', async (req, res) => {
+
     const { address, city, state, country, lat, lng, name, description, price } = req.body;
 
-    const newSpot = await Spot.create({
+    const spot = await Spot.create({
         address, city, state, country, lat, lng, name, description, price
     });
+
     const spots = await Spot.findAll({
-        order: [['id','DESC']],
+        order: [['id', 'DESC']],
         limit: 1
     });
-    // HOW TO GET OWNERid
-    console.log(newSpot)
-    // console.log(spots)
+
+    spots[0].ownerId = req.user.id
     res.json(spots)
 });
 
-router.post('/spots/:spotId/images', async (req, res) => {
+router.post('/:spotId/images', async (req, res) => {
+    const spotId = req.params.spotId;
+
     const { url, preview } = req.body;
 
     const findSpot = await Spot.findByPk(req.params.spotId)
 
-    // console.log(findSpot)
+    // ERROR HANDLING
+    if (findSpot === null) {
+        return res.json({
+                    "message": "Spot couldn't be found",
+                    "statusCode": 404
+                  });
+    };
+    // CREATE NEW IMG
     const newImg = await SpotImage.create({
+        url,
+        preview,
+        spotId: spotId
+    });
 
-    })
-})
+    const foundImg = await SpotImage.findOne({
+        where: {
+            id: newImg.id
+        },
+        attributes: ['id', 'url', 'preview']
+    });
+
+
+    res.json(foundImg)
+});
 
 
 
