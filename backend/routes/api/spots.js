0007d149e46ae61d5953dc1924f19e04a51255ab
michaellacey <mikelacey123123@gmail.com>
2022-09-27 20:01:39 -0400

    finished route get spots of current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
         ],
     })
     const spotsList = [];
-
+    console.log(spots)
     spots.forEach(spot => {
         spotsList.push(spot.toJSON());
     })
@@ -39,54 +39,99 @@ router.get('/', async (req, res) => {
 
         delete spotsList[destroy].Reviews;
         delete spotsList[destroy].SpotImages;
-        destroy++
-    })
-    return res.json(spotsList);
+        destroy++;
+    });
+
+    const newObj = {};
+    newObj.Spots = spotsList;
+    return res.json(newObj);
 
 });
-
+//
 // ALL SPOTS FOR CURRENT USER
+//
 router.get('/current', async (req, res) => {
+    const newArr = [];
+    const userSpots = await Spot.findAll({
+        where: {
+            ownerId: req.user.id
+        },
+        include: [
+            { model: Review },
+            { model: SpotImage }
+        ],
+    });
+    console.log(userSpots)
+    let count = 0;
+    let delCount = 0;
+    userSpots.forEach(ele => {
+        newArr.push(ele.toJSON())
+    });
+    newArr.forEach(ele => {
+        const spotReviews = ele.Reviews;
+        const eleImg = ele.SpotImages;
+        let sum = 0
+        for (let i = 1; i < spotReviews.length + 1; i++) {
+            sum += spotReviews[i - 1].stars;
+            count++;
+        };
+        ele.avgRating = (sum / count).toFixed(1);
 
-})
+        for (let i = 0; i < eleImg.length; i++) {
+            ele.previewImage = eleImg[i].url;
+        };
+        delete newArr[delCount].SpotImages;
+        delete newArr[delCount].Reviews;
+        delCount++;
+        count++;
+    });
+    let newObject = {}
+    newObject.Spots = newArr
+    res.json(newObject)
+});
 
+
+//
 // Post routes
+//
+const validateSignup = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .withMessage("Street address is required"),
+    check('city')
+      .exists({ checkFalsy: true })
+      .withMessage("City is required"),
+    check('state')
+      .exists({ checkFalsy: true })
+      .withMessage("State is required"),
+    check('country')
+      .exists({ checkFalsy: true })
+      .withMessage("Country is required"),
+    // check('lat')
+    //    .isLatLong()
+    //    .withMessage("Latitude is not valid"),
+    // check('lng')
+        // .isLatLong()
+        // .withMessage("Longitude is not valid"),
+    check('name')
+        .isLength({max: 49})
+        .withMessage("Name must be less than 50 characters"),
+    check('description')
+    .exists({ checkFalsy: true })
+        .withMessage("Description is required"),
+    check('price')
+    .exists({ checkFalsy: true })
+        .withMessage("Price per day is required"),
+    handleValidationErrors
+  ];
 
-// const validateSignup = [
-//     check('address')
-//       .exists({ checkFalsy: true })
-//       .withMessage("Street address is required"),
-//     check('city')
-//       .exists({ checkFalsy: true })
-//       .withMessage("City is required"),
-//     check('state')
-//       .exists({ checkFalsy: true })
-//       .withMessage("State is required"),
-//     check('country')
-//       .exists({ checkFalsy: true })
-//       .withMessage("Country is required"),
-//     check('lat')
-//        .withMessage("Latitude is not valid"),
-//     check('lng')
-//         .withMessage("Longitude is not valid"),
-//     check('name')
-//         .isLength({max: 49})
-//         .withMessage("Name must be less than 50 characters"),
-//     check('description')
-//     .exists({ checkFalsy: true })
-//         .withMessage("Description is required"),
-//     check('price')
-//     .exists({ checkFalsy: true })
-//         .withMessage("Price per day is required"),
-//     handleValidationErrors
-//   ];
 //Create new spot
-router.post('/', async (req, res) => {
-
+router.post('/', validateSignup, async (req, res) => {
+    const ownerId = req.user.id
     const { address, city, state, country, lat, lng, name, description, price } = req.body;
 
     const spot = await Spot.create({
-        address, city, state, country, lat, lng, name, description, price
+        ownerId, address, city, state, country, lat, lng, name, description, price
     });
 
     const spots = await Spot.findAll({
@@ -97,7 +142,7 @@ router.post('/', async (req, res) => {
     spots[0].ownerId = req.user.id
     res.json(spots)
 });
-
+// CREATE NEW IMAGE FOR SPOT
 router.post('/:spotId/images', async (req, res) => {
     const spotId = req.params.spotId;
 
@@ -108,9 +153,9 @@ router.post('/:spotId/images', async (req, res) => {
     // ERROR HANDLING
     if (findSpot === null) {
         return res.json({
-                    "message": "Spot couldn't be found",
-                    "statusCode": 404
-                  });
+            "message": "Spot couldn't be found",
+            "statusCode": 404
+        });
     };
     // CREATE NEW IMG
     const newImg = await SpotImage.create({
