6a8e793dd8101415209e87113e04ff93ae65999c
michaellacey <mikelacey123123@gmail.com>
2022-09-29 18:29:59 -0400

    delete a booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -9,45 +9,88 @@ const { handleValidationErrors } = require('../../utils/validation');
 // GET ROUTES
 //
 // GET all bookings for current user
-router.get('/current', async(req,res) => {
+router.get('/current', async (req, res) => {
     const arr = [];
     const bookings = await Booking.findAll({
         where: {
             userId: req.user.id
         },
-        include: [ {model: Spot, attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat','lng','name','price']}]
+        include: [{ model: Spot, attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price'] }]
     });
     bookings.forEach(ele => {
         arr.push(ele.toJSON())
     });
     // Add the preview image urls to all the spot objects
-let count = 0;
-for (let i = 0; i < bookings.length; i++) {
-    let ele = arr[i];
-    let eleId = arr[i].Spot.id;
-    const spots = await SpotImage.findAll({
-        where: {
-            id: eleId
-        },
-        include: [
-            { model: Spot }
-        ]
-    });
-    // Add the url for each ele in the spots object..
-    spots.forEach(ele => {
-        arr[count].Spot.previewImage = ele.url;
-        count++;
-    });
-}
-const bookingsObj = {Bookings:arr}
+    let count = 0;
+    for (let i = 0; i < bookings.length; i++) {
+        let ele = arr[i];
+        let eleId = arr[i].Spot.id;
+        const spots = await SpotImage.findAll({
+            where: {
+                id: eleId
+            },
+            include: [
+                { model: Spot }
+            ]
+        });
+        // Add the url for each ele in the spots object..
+        spots.forEach(ele => {
+            arr[count].Spot.previewImage = ele.url;
+            count++;
+        });
+    }
+    const bookingsObj = { Bookings: arr }
     res.json(bookingsObj);
 });
 
 //
-// POST ROUTES
+// PUT ROUTES
 //
-// router.post('')
+//Edit a booking
+router.put('/:bookingId', async (req, res) => {
+    const { startDate, endDate } = req.body;
 
+    const updateBooking = await Booking.findOne({
+        where: { id: req.params.bookingId }
+    })
+    if (!updateBooking) {
+        res.status(404);
+        return res.json({
+            "message": "Booking couldn't be found",
+            "statusCode": 404
+        })
+    };
+    updateBooking.update({
+        startDate,
+        endDate,
+        userId: req.user.id
+    })
+    await updateBooking.save
+    res.json(updateBooking)
+});
+
+//
+// DESTROY ROUTES
+//
+// Delete a booking
+router.delete('/:bookingId', async (req, res) => {
+    const oldBooking = await Booking.findOne({
+        where: {id: req.params.bookingId}
+    });
+    if (oldBooking === null) {
+        res.status(404);
+        return res.json({
+            "message": "Booking couldn't be found",
+            "statusCode": 404
+        });
+    };
+    await oldBooking.destroy();
+    res.json({
+        "message": "Successfully deleted",
+        "statusCode": 200
+    });
+
+});
 
 
 
