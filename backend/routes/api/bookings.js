ab2808aa9ca13b47978cd89ffd7c2c69d5dfc6e5
michaellacey <mikelacey123123@gmail.com>
2022-09-30 13:32:23 -0400

    revised get bookings for current

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -9,6 +9,8 @@ const { handleValidationErrors } = require('../../utils/validation');
 // GET ROUTES
 //
 // GET all bookings for current user
+
+
 router.get('/current', async (req, res) => {
     const arr = [];
     const bookings = await Booking.findAll({
@@ -20,40 +22,25 @@ router.get('/current', async (req, res) => {
     bookings.forEach(ele => {
         arr.push(ele.toJSON())
     });
+
     // Add the preview image urls to all the spot objects
-    let count = 0;
-    // for (let i = 0; i < bookings.length; i++) {
-    //     let ele = arr[i];
-    //     let eleId = arr[i].Spot.id;
-    //     const spots = await SpotImage.findAll({
-    //         where: {
-    //             id: eleId
-    //         },
-    //         include: [
-    //             { model: Spot }
-    //         ]
-    //     });
-    //     // Add the url for each ele in the spots object..
-    //     spots.forEach(ele => {
-    //         arr[count].Spot.previewImage = ele.url;
-    //         count++;
-    //     });
-    // }
-    for (let i = 0; i < bookings.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         let ele = arr[i];
         let eleId = arr[i].Spot.id;
         const spots = await SpotImage.findOne({
             where: {
-                spotId: arr[0].spotId
+                spotId: eleId
             },
-            // include: [
-            //     { model: Spot }
-            // ]
         });
-        arr[count].Spot.PreviewImage = spots.url
-        count++;
+        if (!spots) {
+
+        }
+        else{
+            arr[i].Spot.PreviewImage = spots.url
+        };
+
     }
-    const bookingsObj = { Bookings: arr }
+    const bookingsObj = { Bookings: arr };
     res.json(bookingsObj);
 });
 
