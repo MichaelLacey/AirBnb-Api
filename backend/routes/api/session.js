26466ccd60619c27de314c97ecf9d2bab211b9ef
michaellacey <mikelacey123123@gmail.com>
2022-09-30 20:11:19 -0400

    refactored auth endpoints

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -56,10 +56,10 @@ router.get(
     (req, res) => {
       const { user } = req;
       if (user) {
-        return res.json({
-          user: user.toSafeObject()
-        });
-      } else return res.json({});
+        return res.json(
+          user.toSafeObject()
+        );
+      } else return res.json();
     }
   );
 
