b5febadf156c54921147426c659bea5bbda7588e
michaellacey <mikelacey123123@gmail.com>
2022-10-17 19:56:04 -0400

    adf

---
+++ frontend/src/store/allSpots.js
@@ -0,0 +1,37 @@
+
+const LOAD_SPOTS = 'load/SPOTS';
+
+export const loadSpots = (spots) => {
+    return {
+        type: LOAD_SPOTS,
+        spots
+    };
+};
+
+/* ___________ T H U N K S   ___________ */
+
+// get all the spots data from backend to be able to render
+export const allSpots = () => async (dispatch) => {
+    const response = await fetch('/api/spots');
+    const spots = await response.json();
+    dispatch(loadSpots(spots.Spots));
+};
+
+/* ___________ R E D U C E R ___________ */
+const allSpotsReducer = (state = {}, action) => {
+    let newState;
+    switch (action.type) {
+        case LOAD_SPOTS:
+            newState = { ...state, ...action.spots }
+            return newState
+
+
+
+
+        default:
+            return state;
+    }
+
+
+};
+export default allSpotsReducer;
